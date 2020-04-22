ef07ced5a3ace3f44384c9d18f71873601bc3470
juliancesaro <42017431+juliancesaro@users.noreply.github.com>
2020-04-23 09:31:36 +1000

    output json in html file

--- hellots/index.js
+++ hellots/index.js
@@ -1,6 +1,9 @@
 "use strict";
 exports.__esModule = true;
+var data = require("./hello.json");
 var fs = require("fs");
-var foo = "<html><head></head><body><p>Hello World!</p></body></html>";
+var foo = "<html><head></head><body><p>" +
+    JSON.stringify(data.$jason) +
+    "</p></body></html>";
 fs.writeFileSync("hellots/index.html", foo);
 //console.log(data.$jason.body);
--- hellots/index.ts
+++ hellots/index.ts
@@ -1,7 +1,10 @@
 import * as data from "./hello.json";
 import * as fs from "fs";
 
-var foo = "<html><head></head><body><p>Hello World!</p></body></html>";
+var foo =
+  "<html><head></head><body><p>" +
+  JSON.stringify(data.$jason) +
+  "</p></body></html>";
 
 fs.writeFileSync("hellots/index.html", foo);
 
