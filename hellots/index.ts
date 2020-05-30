5abdb4779e661fee1581bdec6266799f3bd20505
juliancesaro <julianjcesaro@gmail.com>
2020-05-30 12:16:48 +1000

    Syntax analysis

--- hellots/index.ts
+++ hellots/index.ts
@@ -1,24 +1,66 @@
 import * as data from "./hello.json";
-import * as schema from "./schema.json";
-import * as fs from "fs";
-//import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
+//import * as schema from "./schema.json";
 
-for (const component in data.$jason) {
-  switch(component) { 
-    case "head": { 
-      console.log("head");
-      break; 
-    } 
-    case "body": {  
-      console.log("head");
-      break; 
-    } 
-    default: { 
-      console.log('default');
-      break; 
-    } 
-  } 
-}
+var Validator = require("jsonschema").Validator;
+var v = new Validator();
+
+// Address, to be embedded on Person
+var addressSchema = {
+  id: "/SimpleAddress",
+  type: "object",
+  properties: {
+    lines: {
+      type: "array",
+      items: { type: "string" },
+    },
+    zip: { type: "string" },
+    city: { type: "string" },
+    country: { type: "string" },
+  },
+  required: ["country"],
+};
+
+// Person
+var schema = {
+  id: "/SimplePerson",
+  type: "object",
+  properties: {
+    name: { type: "string" },
+    address: { $ref: "/SimpleAddress" },
+    votes: { type: "integer", minimum: 1 },
+  },
+};
+
+var p = {
+  name: "Barack Obama",
+  m: {
+    mimes: ["1600 Pennsylvania Avenue Northwest"],
+    zip: "DC 20500",
+    city: "Washington",
+    country: "USA",
+  },
+  votes: 3,
+};
+
+v.addSchema(addressSchema, "/SimpleAddress");
+console.log(v.validate(p, schema));
+
+// for (const component in data.$jason) {
+//   switch (component) {
+//     case "head": {
+//       console.log("head");
+//       break;
+//     }
+//     case "body": {
+//       console.log("head");
+//       break;
+//     }
+//     default: {
+//       console.log("default");
+//       break;
+//     }
+//   }
+// }
 
 // let p: string = `<p>${JSON.stringify(data.$jason)}</p>`;
 // const title: string = "<title>Jasonelle app</title>";
