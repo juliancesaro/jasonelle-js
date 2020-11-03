bd6f8d5039b8ee3841e1f1f45a5ed8b195619d1c
juliancesaro <julianjcesaro@gmail.com>
2020-11-04 01:45:25 +1100

    Added command line file specification

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as data from './json/hello.json'
 import * as schema from './json/schema.json'
 import { iterateJason } from './modules/Translator'
 import { iterateIR, compileStyle } from './modules/Generator'
@@ -9,51 +8,56 @@ import * as fs from 'fs'
 var Validator = require('jsonschema').Validator
 var v = new Validator()
 
-// If JSON is valid, create HTML DOM.
-if (v.validate(data.$jason, schema).errors.length > 0) {
-  // Invalid JSON.
-  console.log('JSON is invalid!')
-  console.log(v.validate(data.$jason, schema))
-} else {
-  // Valid JSON.
-  /**
-   * First way of creating application: through creating dom and writing
-   * to html (static)
-   */
+const importFile = async () => {
+  const myData = await import(process.argv.slice(2)[1])
 
-  let IR = iterateJason(data.$jason)
-
-  optimiseStyle(IR)
-
-  let dom = iterateIR(IR)
-
-  let style = compileStyle(IR)
-
-  fs.writeFileSync('src/generated/IR.json', JSON.stringify(IR))
-  fs.writeFileSync('src/generated/index.html', dom.serialize())
-  fs.writeFileSync('src/generated/styles.css', style)
-
-  /**
-   * Second way of creating application: through creating js file and writing
-   * to html (static)
-   */
-
-  // let IR = iterateJason(data.$jason)
-
-  // let optimisedIR = optimiseStyle(IR)
-
-  // let dom = createHTML()
-
-  // let js = render(IR)
-
-  // let style = compileStyle(IR)
-
-  // fs.writeFileSync('src/generated/IR.json', JSON.stringify(IR))
-  // fs.writeFileSync(
-  //   'src/generated/optimisedIR.json',
-  //   JSON.stringify(optimisedIR)
-  // )
-  // fs.writeFileSync('src/generated/index.html', dom.serialize())
-  // fs.writeFileSync('src/generated/script.js', js)
-  // fs.writeFileSync('src/generated/styles.css', style)
+  return myData
 }
+
+importFile().then((data) => {
+  // If JSON is valid, create HTML DOM.
+  if (v.validate(data.$jason, schema).errors.length > 0) {
+    // Invalid JSON.
+    console.log('JSON is invalid!')
+    console.log(v.validate(data.$jason, schema))
+  } else {
+    console.log(process.argv.slice(2)[1])
+
+    // Valid JSON.
+    /**
+     * First way of creating application: through adding to JSDOM and
+     * writing the serialized JSDOM to html file (static)
+     */
+    // let IR = iterateJason(data.$jason)
+
+    // optimiseStyle(IR)
+
+    // let dom = iterateIR(IR)
+
+    // let style = compileStyle(IR)
+
+    // fs.writeFileSync('src/generated/IR.json', JSON.stringify(IR))
+    // fs.writeFileSync('src/generated/index.html', dom.serialize())
+    // fs.writeFileSync('src/generated/styles.css', style)
+
+    /**
+     * Second way of creating application: through creating js file which
+     * adds to the html file DOM at run time (dynamic)
+     */
+
+    let IR = iterateJason(data.$jason)
+
+    optimiseStyle(IR)
+
+    let dom = createHTML()
+
+    let js = render(IR)
+
+    let style = compileStyle(IR)
+
+    fs.writeFileSync('src/generated/IR.json', JSON.stringify(IR))
+    fs.writeFileSync('src/generated/index.html', dom.serialize())
+    fs.writeFileSync('src/generated/script.js', js)
+    fs.writeFileSync('src/generated/styles.css', style)
+  }
+})
